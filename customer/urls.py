0e5afc2e2cb0521124f9b81e72e770e60172c1a1
Rishabh <rishabh.bh22@gmail.com>
2021-04-09 19:11:51 +0530

    added basic views functionalities

--- customer/urls.py
+++ customer/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken import views
 
-from customer.views import registration_view, add_transaction, view_transaction
+from customer.views import registration_view, add_transaction, view_transaction, view_account, view_balance, \
+    filter_transactions
 
 urlpatterns = [
     path('register/', registration_view, name='register'),
     path('login/', views.obtain_auth_token, name='login'),
     path('transaction/add/', add_transaction, name='add_transaction'),
     path('transaction/view/', view_transaction, name='view_transaction'),
-
+    path('transaction/filter/', filter_transactions, name='filter_transactions'),
+    path('account/view/', view_account, name='view_account'),
+    path('account/balance/', view_balance, name='view_balance'),
 ]
